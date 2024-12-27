d5f5d8af32011da706df3fb099e8867a220f890b
Shree <shreebiradar2005@gmail.com>
2024-12-27 16:49:32 +0530

    Removing the temp class
    The username and password is hidden

--- java/Ecommerce/SQL_Data.java
+++ java/Ecommerce/SQL_Data.java
@@ -2,6 +2,6 @@ package Ecommerce;
 
 public class SQL_Data {
 	final static String url = "jdbc:mysql://127.0.0.1:3306/product_management";
-	final static String username = "root";
-	final static String password = "Shree@123";
+	final static String username = "****"; // For data hiding the username and password is hidden
+	final static String password = "*********";
 }
