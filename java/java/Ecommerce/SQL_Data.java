5517c9513344afa0b8269ed541dceb7b6a595f4d
Shree <shreebiradar2005@gmail.com>
2025-01-03 00:07:58 +0530

    fix: removed the extra package name as java

--- java/java/Ecommerce/SQL_Data.java
+++
@@ -1,7 +0,0 @@
-package Ecommerce;
-
-public class SQL_Data {
-	final static String url = "jdbc:mysql://127.0.0.1:3306/product_management";
-	final static String username = "root";
-	final static String password = "Shree@123";
-}
